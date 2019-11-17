b0123fec3753ce749cead3161a671d819bd5ec31
benneee <benedictiknkeonye@gmail.com>
2019-11-17 12:44:31 +0100

    Yes! Populated the damn DB

--- local-library/models/bookinstance.js
+++ local-library/models/bookinstance.js
@@ -7,7 +7,7 @@ var BookInstanceSchema = new Schema({
   status: {
     type: String,
     required: true,
-    enum: ['Available', 'Maintenance', 'Loaded', 'Reserved'],
+    enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
     default: 'Maintenance'
   },
   due_back: { type: Date, default: Date.now }
